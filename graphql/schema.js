5acf5315d4e890e2796fc2166be6df8ce0026348
hendry Lioenardi <astrobemo@gmail.com>
2023-12-08 15:38:22 +0700

    update schema for other beside user

--- graphql/schema.js
+++ graphql/schema.js
@@ -3,27 +3,31 @@ import { buildSchema } from 'graphql';
 const schema = buildSchema(`#graphql
     type User {
         id: ID!
-        username: String!,
-        posisi_id: Int!,
-        status_aktif: Boolean!,
-        time_start: String!,
-        time_end: String!,
+        username: String!
+        posisi_id: Int!
+        status_aktif: Boolean!
+        time_start: String!
+        time_end: String!
     }
     type Barang {
         id: ID!
         nama_jual: String!
+        status_aktif: Boolean!
     }
     type Warna {
         id: ID!
         warna_jual: String!
+        status_aktif: Boolean!
     }
     type Customer {
         id: ID!
         nama: String
+        status_aktif: Boolean!
     }
     type Supplier {
         id: ID!
         nama: String!
+        status_aktif: Boolean!
     }    
     type Pembelian {
         id: ID!
