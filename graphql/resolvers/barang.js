53393171a8afabe6d99b30f4b30edab165ed6f37
hendry Lioenardi <astrobemo@gmail.com>
2023-12-12 16:34:11 +0700

    change schme, comment out barang

--- graphql/resolvers/barang.js
+++ graphql/resolvers/barang.js
@@ -1,28 +1,29 @@
-import getPoolForRequest from "../../config/mysqlCon.js";
+// import getPoolForRequest from "../../config/mysqlCon.js";
 
-const getBarang = {
-    barang: async(args, req)=>{
-      const pool = getPoolForRequest(req);
-        try {
-          const query = `SELECT * FROM nd_barang WHERE id = ?`;
-          const [rows] = await pool.query(query, [args.id]);
-          return rows[0];
-        } catch (error) {
-          console.error(error);
-          throw new Error("Internal Server Error Barang Single");
-        }
-    },
-    barangs: async(args, req)=>{
-      const pool = getPoolForRequest(req);
-        try {
-          const query = 'SELECT * FROM nd_barang';
-          const [rows] = await pool.query(query);
-          return rows;
-        } catch (error) {
-          console.error(error);
-          throw new Error("Internal Server Error Barang All");
-        }
-    }
-}
+// const getBarang = {
+//     barang: async(args, req)=>{
+//       const pool = getPoolForRequest(req);
+//         try {
+//           const query = `SELECT * FROM nd_barang WHERE id = ?`;
+//           const [rows] = await pool.query(query, [args.id]);
+//           return rows[0];
+//         } catch (error) {
+//           console.error(error);
+//           throw new Error("Internal Server Error Barang Single");
+//         }
+//     },
+//     barangs: async(args, req)=>{
+//       const pool = getPoolForRequest(req);
+//         try {
+//           const query = 'SELECT * FROM nd_barang';
+//           const [rows] = await pool.query(query);
+//           return rows;
+//         } catch (error) {
+//           console.error(error);
+//           throw new Error("Internal Server Error Barang All");
+//         }
+//     }
+// }
 
-export default getBarang;
+// export default getBarang;
+export default null;
