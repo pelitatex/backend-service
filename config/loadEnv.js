e5d0cd97d91fa744563a5154ae00022b81742344
astrobemo <astrobemo@gmail.com>
2024-08-30 10:12:33 +0700

    fix config/loadEnv file

--- config/loadEnv.js
+++ config/loadEnv.js
@@ -1,5 +1,16 @@
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load the root .env file first
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+// Now, load the environment-specific .env file
 const env = process.env.NODE_ENV || 'development';
-dotenv.config({ path: `.env.${env}` });
+dotenv.config({ path: path.resolve(__dirname, `../.env.${env}`) });
 
 export default env;
