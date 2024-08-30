b45d3776f45977f9546b50c790a821344037cdbc
astrobemo <astrobemo@gmail.com>
2024-08-30 14:01:15 +0700

    update server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 // const port = process.env.PORT || 3000;
 import gateway from './api-gateway.js';
 import app from './index.js';
+import { PORT_GATEWAY, PORT_APP } from './config/loadEnv.js';
 
-const port_gw = process.env['PORT_GATEWAY']
-const port_user = process.env['PORT_APP'];
+const port_gw = PORT_GATEWAY
+const port_user = PORT_APP;
 
 gateway.listen(port_gw, () => {
   console.log(`Gateway is running on port ${port_gw}`);
