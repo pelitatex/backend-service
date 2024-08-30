d01e9dfcd4686591eaeefa8d1bb8a7cac96d2b1c
astrobemo <astrobemo@gmail.com>
2024-08-30 13:35:08 +0700

    add log on loadEnv

--- config/loadEnv.js
+++ config/loadEnv.js
@@ -21,10 +21,17 @@ loadEnvFile(path.resolve(__dirname, '../.env'));
 const envMain = process.env.NODE_ENV || 'development';
 loadEnvFile(path.resolve(__dirname, `../.env.${envMain}`));
 
+
 export const LIFETIME = process.env.TOKEN_LIFETIME;
 export const TOKENSECRET = process.env.TOKEN_SECRET;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
 export const PORT_APP = process.env.PORT_APP;
 export const PORT_GATEWAY = process.env.PORT_GATEWAY;
 export const ENVIRONMENT = envMain;
+
+if (envMain === 'development' || envMain === 'testing') {
+  console.log('Development environment detected');
+  console.log(LIFETIME, TOKENSECRET, FRONTEND_URL, PORT_APP, PORT_GATEWAY, ENVIRONMENT);
+}
+
 
