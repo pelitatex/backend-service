8366127c255562984d838f577ea3169febd76018
astrobemo <astrobemo@gmail.com>
2025-01-20 11:23:50 +0700

    change env.config.js as module

--- env.config.js
+++ env.config.js
@@ -1,33 +1,23 @@
-const { fileURLToPath } = require('url');
-const { dirname } = require('path');
-
-// PM2 requires CommonJS by default. Add this flag in your PM2 start command: `--interpreter node --node-args="--experimental-loader=@esbuild-kit/esm-loader"`
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Export the configuration object
-const configuration = {
-  apps: [
-    {
-      name: "pelita-api-app",
-      script: "./index.js",
-      env: {
-        NODE_ENV: "development",
+export default {
+    apps: [
+      {
+        name: "pelita-api-app",
+        script: "./index.js",
+        env: {
+          NODE_ENV: "development",
+        },
+        env_development: {
+          ENV_FILE: `.env.development`,
+        },
+        env_testing: {
+          NODE_ENV: "testing",
+          ENV_FILE: `.env.testing`,
+        },
+        env_production: {
+          NODE_ENV: "production",
+          ENV_FILE: `.env.production`,
+        },
       },
-      env_development: {
-        ENV_FILE: `.env.development`,
-      },
-      env_testing: {
-        NODE_ENV: "testing",
-        ENV_FILE: `.env.testing`,
-      },
-      env_production: {
-        NODE_ENV: "production",
-        ENV_FILE: `.env.production`,
-      },
-    },
-  ],
-};
-
-module.exports = configuration;
+    ],
+  };
+  
