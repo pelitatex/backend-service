7cc8f30a613afe171fac391d554ca04f6a2f3d0e
astrobemo <astrobemo@gmail.com>
2025-03-03 16:46:42 +0700

    add barang sku toko add and update

--- graphql/resolvers/barangSKUToko.js
+++ graphql/resolvers/barangSKUToko.js
@@ -1,25 +1,25 @@
-import { NODE2_URL } from '../../config.js';
-import axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
+// import queryLogger from "../../helpers/queryTransaction.js";
+import queryTransaction from '../../helpers/queryTransaction.js';
+import barangSKUResolver from './barangSKU.js';
 
 const barangSKUResolver = {
   Query:{
     barangSKUToko: async(_,args, context)=>{
-      const toko_id = args.toko_id;
-      try {
-        const otherAppUrl = `${NODE2_URL}/get_barang_sku_by_toko?id=${toko_id}`;
-
-        axios.get(otherAppUrl)
-          .then(response => {
-              res.json(response.data);
-          })
-          .catch(error => {
-              console.error(`Error fetching data from other app: ${error.message}`);
-              res.status(500).json({ error: 'Failed to fetch data from other app' });
-          });
-
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data from other app' });
-    }
+      const pool = context.pool;
+        if (!pool) {
+          console.log('context', pool);
+          throw new Error('Database pool not available in context.');
+        }
+        try {
+          context.useSatuanLoader = true;
+          const query = `SELECT * FROM nd_toko_barang_sku WHERE toko_id = ?`;
+          const [rows] = await pool.query(query, [args.toko_id]);
+          return rows[0];
+        } catch (error) {
+          console.error(error);
+          throw new Error("Internal Server Error");
+        }
     },
     allBarangSKUToko: async(_,args, context)=>{
       const pool = context.pool;
@@ -29,12 +29,12 @@ const barangSKUResolver = {
         }
         try {
           context.useSatuanLoader = false;
-          const query = 'SELECT * FROM nd_barang_sku';
+          const query = 'SELECT * FROM nd_toko_barang_sku';
           const [rows] = await pool.query(query);
           return rows;
         } catch (error) {
           console.error(error);
-          throw new Error("Internal Server Error Barang SKU All");
+          throw new Error("Internal Server Error Barang SKU TOKO All");
         }
     }
   },
@@ -46,71 +46,26 @@ const barangSKUResolver = {
         throw new Error('Database pool not available in context.');
       }
 
-      const newItems = [];
+      const {toko_id, barang_sku_id} = input;
 
+      const checkQuery = 'SELECT * FROM nd_toko_barang_sku WHERE toko_id = ? and barang_sku_id = ?';
+      const [checkRows] = await pool.query(checkQuery, [toko_id, barang_sku_id]);
+      if (checkRows.length > 0) {
+        throw new Error('Toko sudah punya barang sku.');
+      }
       
       try {
-        for (const item of input) {
-          
-          const { barang_id, warna_id, satuan_id, status_aktif } = item;
-    
-          const getNamaBarangQuery = 'SELECT nama_jual as nama FROM nd_barang WHERE id = ?';
-          const [namaBarangRows] = await pool.query(getNamaBarangQuery, [barang_id]);
-          const nama = namaBarangRows[0].nama;
-    
-          const getWarnaJualQuery = 'SELECT warna_jual FROM nd_warna WHERE id = ?';
-          const [warnaJualRows] = await pool.query(getWarnaJualQuery, [warna_id]);
-          const warna_jual = warnaJualRows[0].warna_jual;
-    
-          
-          const getSatuanQuery = 'SELECT nama FROM nd_satuan WHERE id = ?';
-          const [satuanRows] = await pool.query(getSatuanQuery, [satuan_id]);
-          const nama_satuan = satuanRows[0].nama;
-    
-    
-          const nama_jual = nama.toUpperCase()+' '+warna_jual.toUpperCase();
-          const nama_barang = nama.toUpperCase()+' '+warna_jual.toUpperCase()+' '+nama_satuan.toUpperCase();
-    
-          const checkExistQuery = 'SELECT * FROM nd_barang_sku WHERE nama_barang = ?';
-          const [existRows] = await pool.query(checkExistQuery, [nama_barang]);
-          if (existRows.length > 0) {
-            throw new Error('Barang SKU already exists.');
-          }
-    
-          const barangIdStr = String(barang_id).padStart(2, '0');
-          const warnaIdStr = String(warna_id).padStart(2, '0');
-          const satuanIdStr = String(satuan_id).padStart(2, '0');
-          const sixDigitIdentifier = barangIdStr + warnaIdStr + satuanIdStr;
-          const kode = uuidv4().substring(0, 13);
-          const sku_id = sixDigitIdentifier +'-'+ kode;
-          newItems.push([sku_id, nama_barang, nama_jual, barang_id, warna_id, satuan_id, status_aktif]);
 
-        };
-
-        const namaBarangSet = new Set();
-        for (const item of newItems) {
-          if (namaBarangSet.has(item[1])) {
-            throw new Error('Duplicate nama_barang found in input.');
-          }
-          namaBarangSet.add(item[1]);
+        const query = `INSERT INTO nd_toko_barang_sku (toko_id, barang_sku_id) VALUES  (?,?) `;
+        const [insertQuery] = await pool.query(query, [toko_id, barang_sku_id])
+        if (insertQuery[0].count > 0) {
+          throw new Error('Add toko barang sku failed');
         }
+        return true;
 
-
-        const placeholder = newItems.map(() => '(?, ?, ?, ?, ?, ?, ?)').join(', ');
-        const query = `INSERT INTO nd_barang_sku (sku_id, nama_barang, nama_jual, barang_id, warna_id, satuan_id, status_aktif) VALUES ${placeholder}`;
-
-        // const params = [sku_id, nama_barang, nama_jual, barang_id, warna_id, satuan_id, status_aktif];
-        const params = newItems.flat();
-        const result = await queryTransaction.insert(context, "nd_barang_sku", query, params);
-        return result;
-
-        /* const [result] = await pool.query(query, [sku_id, nama_barang, nama_jual, barang_id, warna_id, satuan_id, status_aktif]);
-        const insertedId = result.insertId;
-        queryLogger(pool, `nd_barang_sku`, result.insertId, query, [sku_id, nama_barang, nama_jual, barang_id, warna_id, satuan_id, status_aktif]);
-        return {id: insertedId, sku_id, nama_barang, nama_jual, barang_id, warna_id, satuan_id, status_aktif};*/
       } catch (error) { 
         console.error(error);
-        throw new Error(error.message || 'Internal Server Error Add Barang');
+        throw new Error(error.message || 'Internal Server Error Add toko barang sku');
       }
     },
     updateBarangSKUToko: async (_, {id, input}, context) => {
@@ -119,49 +74,15 @@ const barangSKUResolver = {
         console.log('context', pool);
         throw new Error('Database pool not available in context.');
       }
-
-      const { nama_barang, nama_jual, status_aktif } = input;
       try {
-        const checkExistQuery = 'SELECT * FROM nd_barang_sku WHERE nama_barang = ? and id <> ?';
-        const [existRows] = await pool.query(checkExistQuery, [nama_barang, id]);
-        
-        if (existRows.length > 0) {
-          throw new Error('Barang SKU already exists.');
-        }
-
-        if(nama_jual === null || nama_jual === '') {
-          const getNamaBarangQuery = 'SELECT nama_jual as nama FROM nd_barang WHERE id = ?';
-          const [namaBarangRows] = await pool.query(getNamaBarangQuery, [barang_id]);
-          const nama = namaBarangRows[0].nama;
-
-          const getWarnaJualQuery = 'SELECT warna_jual FROM nd_warna WHERE id = ?';
-          const [warnaJualRows] = await pool.query(getWarnaJualQuery, [warna_id]);
-          const warna_jual = warnaJualRows[0].warna_jual;
-
-          nama_jual = nama.toUpperCase()+' '+warna_jual.toUpperCase();
-        }
-
-        const query = 'UPDATE nd_barang_sku SET nama_barang = ?, nama_jual = ?, status_aktif = ? WHERE id = ?';
-        const params = [nama_barang.toUpperCase(), nama_jual.toUpperCase(), status_aktif, id];
-        const result = await queryTransaction.update(context, "nd_barang_sku", id, query, params);
-        return result;
-
-        /* const [result] = await pool.query(query, [nama_barang.toUpperCase(), nama_jual.toUpperCase(), status_aktif, id]);
-        if (result.affectedRows === 0) {
-          throw new Error('Barang SKU not found');
-        }
-        queryLogger(pool, `nd_barang_sku`, id, query, [nama_barang.toUpperCase(), nama_jual.toUpperCase(), status_aktif, id]);
-
-        const updatedQuery = `SELECT * FROM nd_barang_sku WHERE id = ?`;
-        const [rows] = await pool.query(updatedQuery, [id]);
-        return rows[0]; */
+        throw new Error('Internal Server Toko SKU Barang tidak bisa di edit');
 
       } catch (error) {
         console.error(error);
         throw new Error('Internal Server Error Update Barang');
       }
     },
-  }
+  },
 }
 
 export default barangSKUResolver;
