c7def23fe3747d40f92e6a71dd76002d7ebb799f
astrobemo <astrobemo@gmail.com>
2025-03-14 11:24:34 +0700

    add confirm channel to producers

--- helpers/producers.js
+++ helpers/producers.js
@@ -2,6 +2,7 @@ import { connect } from "amqplib";
 import { RABBITMQ_URL, RABBITMQ_USER, RABBITMQ_PASSWORD } from "../config/loadEnv.js";
 
 let channel;
+let confirmChannel;
 const connection = await connect(`amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@${RABBITMQ_URL}:5672/master`).catch((err) => {
     console.error(err);
     // process.exit(1);
@@ -9,17 +10,28 @@ const connection = await connect(`amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@$
 
 if (connection) {
     channel = await connection.createChannel();
+    confirmChannel = await connection.createConfirmChannel();
+    console.log('Connected to RabbitMQ');
 }
 //const channel = await connection.createChannel();
 
 
-export const publishExchange = async (exchange, routingKey, message, timeout = 0) => {
+export const publishExchange = async (exchange, routingKey, message, timeout = 0, needConfirm = false) => {
     if (connection === undefined) {
         console.error('No connection');
         return;
-        
     }
     console.log(`Publishing message to exchange ${exchange} with routing key ${routingKey}`);
+
+    if(needConfirm) {
+        if(timeout > 0) {
+            channelConfirm.publish(exchange, routingKey, message, {persistent:true,expiration: timeout});
+            return;
+        }
+        channelConfirm.publish(exchange, routingKey, message, {persistent:true});
+        return;
+    };
+
     if(timeout > 0) {
         channel.publish(exchange, routingKey, message, {persistent:true,expiration: timeout});
         return;
@@ -28,13 +40,22 @@ export const publishExchange = async (exchange, routingKey, message, timeout = 0
 }; 
 
 
-export const sendToQueue = async (queue, message, timeout = 0) => {
+export const sendToQueue = async (queue, message, timeout = 0, needConfirm = false) => {
     if (connection === undefined) {
         console.error('No connection');
         return;
         
     }
     console.log(`Sending message to queue ${queue}`);
+
+    if(needConfirm) {
+        if (timeout > 0) {
+            confirmChannel.sendToQueue(queue, message, {persistent:true, expiration: timeout});
+            return;
+        }
+        confirmChannel.sendToQueue(queue, message, {persistent:true});
+        return;
+    }
 
     if (timeout > 0) {
         channel.sendToQueue(queue, message, {persistent:true, expiration: timeout});
