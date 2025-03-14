31a094637a18b79ee11b05e90998d36d5af91cba
astrobemo <astrobemo@gmail.com>
2025-03-14 16:42:37 +0700

    add function  registerBarangSKUToko to regiserBarangTOKo

--- helpers/registerBarangToko.js
+++ helpers/registerBarangToko.js
@@ -3,6 +3,18 @@ import { sendToQueue } from "./producers";
 export const registerBarangSKUToko = async (tokoAlias, toko_id, barang_id, pool) => {
 
     const pool = context.pool;
-    
+    if (!pool) {
+        console.log('context', pool);
+        throw new Error('Database pool not available in context.');
+    }
+    try {
+        
+        await sendToQueue(`pairing_barang_master_toko`, Buffer.from(JSON.stringify({tokoAlias, toko_id, barang_id})), 0 , true );
+        
+
+    } catch (error) {
+        console.error(error);
+        throw new Error('Internal Server Error get barangsku toko');
+    }
     
 }
