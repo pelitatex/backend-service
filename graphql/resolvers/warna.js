96904e578b84e8ac1b48c6e21785ccfc433d3367
astrobemo <astrobemo@gmail.com>
2025-03-15 18:14:10 +0700

    refaktor query warna

--- graphql/resolvers/warna.js
+++ graphql/resolvers/warna.js
@@ -1,39 +1,19 @@
-// import queryLogger from "../../helpers/queryTransaction.js";
-import { queryTransaction } from "../../helpers/queryTransaction.js";
+import queryLogger from "../../helpers/queryTransaction.js";
+//import { queryTransaction } from "../../helpers/queryTransaction.js";
+import handleResolverError from "../handleResolverError.js";
 
 const warnaResolver = {
   Query : {
-    warna: async(_,args, context)=>{
-      const pool = context.pool;
-      if (!pool) {
-        console.log('context', pool);
-        throw new Error('Database pool not available in context.');
-      }
-        try {
-            const query = `SELECT * FROM nd_warna WHERE id = ?`;
-            const [rows] = await pool.query(query, [args.id]);
-            return rows[0];
-        } catch (error) {
-          console.error(error);
-          throw new Error("Internal Server Error Warna Single");
-        }
-    },
-    allWarna: async(_,args, context)=>{
-      const pool = context.pool;
-        if (!pool) {
-          console.log('context', pool);
-          throw new Error('Database pool not available in context.');
-        }
-
-        try {
-            const query = 'SELECT * FROM nd_warna';
-            const [rows] = await pool.query(query);
-            return rows;
-        } catch (error) {
-          console.error(error);
-          throw new Error("Internal Server Error Warna All");
-        }
-    }
+    warna: handleResolverError(async(_,args, context)=>{
+      const query = `SELECT * FROM nd_warna WHERE id = ?`;
+      const [rows] = await pool.query(query, [args.id]);
+      return rows[0];
+    }),
+    allWarna: handleResolverError(async(_,args, context)=>{
+      const query = 'SELECT * FROM nd_warna';
+      const [rows] = await pool.query(query);
+      return rows;
+    })
   },
   Mutation: {
     addWarna: async (_, {input}, context) => {
