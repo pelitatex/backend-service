503251404ea0fdadcd97151d230d95c537bb464b
astrobemo <astrobemo@gmail.com>
2025-03-15 18:08:47 +0700

    update satuanResolver mutation

--- graphql/resolvers/satuan.js
+++ graphql/resolvers/satuan.js
@@ -16,48 +16,24 @@ const satuanResolver = {
     })
   },
   Mutation: {
-    addSatuan: async (_, {input}, context) => {
-      try {
-        const pool = context.pool;
-        if (!pool) {
-          console.log('context', pool);
-          throw new Error('Database pool not available in context.');
-        }
-        const { nama, status_aktif } = input;
-        const query = 'INSERT INTO nd_satuan (nama, status_aktif) VALUES (?, ?)';
+    addSatuan: handleResolverError(async (_, {input}, context) => {
+      const { nama, status_aktif } = input;
+      const query = 'INSERT INTO nd_satuan (nama, status_aktif) VALUES (?, ?)';
 
-        const result = await queryTransaction.insert(context, 'nd_satuan', query, [nama, status_aktif]);
-        /* const [result] = await pool.query(query, [nama, status_aktif]);
-        queryLogger(pool, 'nd_satuan', result.insertId ,query, [nama, status_aktif]); */
-        return { id: result.id, nama, status_aktif };
-      } catch (error) {
-        console.error(error);
-        throw new Error(error.message || "Internal Server Error Add Satuan");
+      const [result] = await pool.query(query, [nama, status_aktif]);
+      queryLogger(pool, 'nd_satuan', result.insertId ,query, [nama, status_aktif]);
+      return { id: result.id, nama, status_aktif };
+    }),
+    updateSatuan: handleResolverError(async (_, {id, input}, context) => {
+      const { nama, status_aktif } = input;
+      const query = 'UPDATE nd_satuan SET nama = ?, status_aktif = ? WHERE id = ?';
+      const [result] = await pool.query(query, [nama, status_aktif, id]);
+      if (result.affectedRows === 0) {
+        throw new Error(`Satuan with id ${id} not found.`);
       }
-    },
-    updateSatuan: async (_, {id, input}, context) => {
-      try {
-        const pool = context.pool;
-        if (!pool) {
-          console.log('context', pool);
-          throw new Error('Database pool not available in context.');
-        }
-        const { nama, status_aktif } = input;
-        const query = 'UPDATE nd_satuan SET nama = ?, status_aktif = ? WHERE id = ?';
-        const result = await queryTransaction.update(context, 'nd_satuan', query, [nama, status_aktif, id]);
-        return result;
-
-        /* const [result] = await pool.query(query, [nama, status_aktif, id]);
-        if (result.affectedRows === 0) {
-          throw new Error(`Satuan with id ${id} not found.`);
-        }
-        queryLogger(pool, 'nd_satuan' ,query, [nama, status_aktif,id]);
-        return { id : id, nama, status_aktif }; */
-      } catch (error) {
-        console.error(error);
-        throw new Error(error.message || "Internal Server Error Update Satuan");
-      }
-    }
+      queryLogger(pool, 'nd_satuan' ,query, [nama, status_aktif,id]);
+      return { id : id, nama, status_aktif }; 
+    })
   }
 }
 
