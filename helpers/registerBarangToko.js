e8ee5adec1bfd37d8dee694e3d252cf6f166283f
astrobemo <astrobemo@gmail.com>
2025-03-15 15:49:58 +0700

    add callbackconfirm in registerBarangTOko

--- helpers/registerBarangToko.js
+++ helpers/registerBarangToko.js
@@ -1,15 +1,23 @@
 import { sendToQueue } from "./producers";
 
+const callbackConfirm = (err, ok) => {
+    if (err) {
+        console.error('Message nacked');
+    } else {
+        console.log('Message acked');
+    }
+}
+
 export const initBarangMasterToko = async (tokoAlias, toko_id, barang_id, pool) => {
 
-    const pool = context.pool;
     if (!pool) {
         console.log('context', pool);
         throw new Error('Database pool not available in context.');
     }
     try {
         
-        await sendToQueue(`pairing_barang_master_toko`, Buffer.from(JSON.stringify({tokoAlias, toko_id, barang_id})), 0 , true );
+        await sendToQueue(`pairing_barang_master_toko`, Buffer.from(JSON.stringify({tokoAlias, toko_id, barang_id})), 0 , true, callbackConfirm);
+        return;
         
     } catch (error) {
         console.error(error);
