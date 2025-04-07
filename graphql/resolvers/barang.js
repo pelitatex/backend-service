3b45ee8c4fd1b58f8d4fbdd6308e7d7b5c56bcd7
astrobemo <astrobemo@gmail.com>
2025-04-07 16:40:21 +0700

    update addbarang resolver

--- graphql/resolvers/barang.js
+++ graphql/resolvers/barang.js
@@ -5,7 +5,7 @@ import handleResolverError from "../handleResolverError.js";
 const barangResolver = {
     Query: {
         barang: handleResolverError(async(_,args, context)=>{
-        const pool = context.pool;
+            const pool = context.pool;
             
             const query = `SELECT * FROM nd_barang WHERE id = ?`;
             const [rows] = await pool.query(query, [args.id]);
@@ -14,6 +14,7 @@ const barangResolver = {
         }),
         allBarang: handleResolverError(async (_, args, context) => {
             
+            const pool = context.pool;
             const query = `SELECT * FROM nd_barang`;
             const [rows] = await pool.query(query);
             return rows;
@@ -23,10 +24,6 @@ const barangResolver = {
     Mutation: {
         addBarang: handleResolverError(async (_, {input}, context) => {
             const pool = context.pool;
-            if (!pool) {
-                console.log('context', pool);
-                throw new Error('Database pool not available in context.');
-            }
             const {
                 sku_id,
                 nama_jual,
@@ -42,6 +39,7 @@ const barangResolver = {
                 status_aktif
 
             } = input;
+            
 
             const query = `INSERT INTO nd_barang (sku_id, nama_jual, satuan_id, jenis_barang, grade, bahan, tipe, fitur, qty_warning, deskripsi_info, status_aktif) 
                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
@@ -60,22 +58,42 @@ const barangResolver = {
                 status_aktif
             ];
 
-            console.log('params', params);
-
-            // const [result] = await pool.query(query, params);
             
-            const result = await queryTransaction.insert(context, "nd_barang", query, params);
+            // const result = await queryTransaction.insert(context, "nd_barang", query, params);
 
-            if (nama_beli != '') {
-                const queryBeli = `INSERT INTO nd_barang_beli (nama, barang_id, status_aktif ) VALUES (?, ?)`;
-                const resultBeli = await queryTransaction.insert(context, "nd_barang_beli", queryBeli, [nama_beli, result.id, 1]);
+            let insertId = null;
+
+            try {
+                pool.query("START TRANSACTION");
+                const [result] = await pool.query(query, params);
+                if (result.affectedRows == 0) {
+                    throw new Error('Gagal Insert Barang');
+                }
+                insertId = result.insertId;
+    
+                if (nama_beli != '') {
+                    const queryBeli = `INSERT INTO nd_barang_beli (nama, barang_id, status_aktif ) VALUES (?, ?)`;
+                    const [resultBeli] = await pool.query(queryBeli, [nama_beli, result.id, 1]);
+                    if(resultBeli.affectedRows == 0){
+                        throw new Error('Gagal Insert Barang Beli');
+                    }
+                }
+
+                pool.query("COMMIT");
                 
+            } catch (error) {
+                pool.query("ROLLBACK");
+                throw error;
             }
+
+
+            queryLogger(pool, `nd_barang`, insertId, query, params);
             
-            return {id: result.id,
+            return {id: insertId,
                 sku_id,
                 nama_jual,
                 satuan_id,
+                
                 jenis_barang,
                 grade,
                 bahan,
