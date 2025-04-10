67e0a94e3159b9c816167da0a52f29565e1b2edc
astrobemo <astrobemo@gmail.com>
2025-04-10 14:02:37 +0700

    update supplier unite test

--- graphql/resolvers/supplier.js
+++ graphql/resolvers/supplier.js
@@ -5,11 +5,13 @@ import handleResolverError from "../handleResolverError.js";
 const supplierResolver = {
   Query:{
     supplier: handleResolverError(async(_,args, context)=>{
+      const pool = context.pool;
       const query = `SELECT * FROM nd_supplier WHERE id = ?`;
         const [rows] = await pool.query(query, [args.id]);
         return rows[0];
     }),
     allSupplier: handleResolverError(async(_,args, context)=>{
+      const pool = context.pool;
       const query = 'SELECT * FROM nd_supplier';
       const [rows] = await pool.query(query);
       return rows;
@@ -17,6 +19,7 @@ const supplierResolver = {
   },
   Mutation: {
     addSupplier: handleResolverError(async (_, {input}, context) => {
+      const pool = context.pool;
 
       const {
         kode,
@@ -52,16 +55,29 @@ const supplierResolver = {
         ) VALUES (?,?,?,?, ?,?,?,?, ?,?,?,?)`;
 
         const params = [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif];
-        const [result] = await pool.query(query, [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif]);
-        queryLogger(pool, `nd_supplier`, result.insertId, query, [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif]);
+        let insertId = null;
+        try {
+          pool.query('START TRANSACTION');
+          const [result] = await pool.query(query, params);
+          if(result.affectedRows === 0) {
+            throw new Error('Failed to insert data into nd_supplier');
+          }
+          insertId = result.insertId;
+          pool.query ('COMMIT');
+        } catch (error) {
+          pool.query('ROLLBACK');
+          throw error;
+        }
+        queryLogger(pool, `nd_supplier`, insertId, query, [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif]);
 
-        return { id: result.id, kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif };
+        return { id: insertId, kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif };
       } catch (error) {
         console.error(error);
         throw new Error("Internal Server Error Add Supplier");
       }
     }),
     updateSupplier: handleResolverError(async (_, {id, input}, context) => {
+      const pool = context.pool;
 
       const {
         kode,
@@ -94,9 +110,18 @@ const supplierResolver = {
           WHERE id = ?`;
 
         const params = [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif, id];
-        const [result] = await pool.query(query, [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif, id]);
-        if (result.affectedRows === 0) {
-          throw new Error("Supplier not found");
+        try {
+          
+          pool.query('START TRANSACTION');
+          const [result] = await pool.query(query, [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif, id]);
+          if (result.affectedRows === 0) {
+            throw new Error("Supplier not found");
+          }
+          pool.query('COMMIT');
+
+        }catch (error) {
+          pool.query('ROLLBACK');
+          throw error;
         }
         queryLogger(pool, `nd_supplier`, id, query, [kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif]);
         return { id, kode, nama, alamat, telepon, fax, kota, kode_pos, nama_bank, no_rek_bank, email, website, status_aktif };
